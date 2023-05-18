013458efd6022b3d72296791176c1bfa3588122a
Gracerue <munyaradzi045@gmail.com>
2023-05-18 16:00:12 +0200

    Fix linter errors

---
+++ my_enumerable.rb
@@ -0,0 +1,18 @@
+module MyEnumerable
+  def all?
+    each { |element| return false unless yield(element) } if block_given?
+    true
+  end
+
+  def any?
+    each { |element| return true if yield(element) } if block_given?
+    false
+  end
+
+  def filter
+    filter_array = []
+    each { |item| filter_array << item if yield(item) } if block_given?
+    filter_array
+  end
+end
+  
