0c59fe694a03bc37c922589c41444ba180b67f87
Gracerue <munyaradzi045@gmail.com>
2023-05-18 14:29:28 +0200

    Initialize a @list

---
+++ myList.rb
@@ -0,0 +1,22 @@
+require_relative 'myEnumerable'
+
+class MyList
+  include MyEnumerable
+
+  def initialize(*list)
+    @list = list
+  end
+
+  def each(&block)
+    @list.each(&block)
+  end
+end
+
+list = MyList.new(1, 2, 3, 4);
+
+puts list.all? { |element| element < 5}
+puts list.all? { |element| element > 5}
+puts list.any? { |element| element == 2}
+puts list.any? { |element| element == 5}
+filter_array = list.filter { |element| element.even?}
+puts filter_array.inspect
